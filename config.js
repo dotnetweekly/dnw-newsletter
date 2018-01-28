90d3c76040b1d616f834aea1da0ef4fbe3cf9159
bstavroulakis <bstavroulakis@gmail.com>
2018-01-28 00:10:16 -0800

    change api domain

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const apiDomain = process.env.API_DOMAIN || 'http://localhost:3000/api/v1/';
+const apiDomain = process.env.DOMAIN_API || 'http://localhost:3000/api/v1/';
 const newsletterDomain = process.env.NEWSLETTER_DOMAIN || 'http://localhost:6100/';
 const clientDomain = process.env.CLIENT_DOMAIN || 'http://localhost:5000/';
 
