8b0bc074fbd4afb10ff9d8cb735945ad299d757f
bstavroulakis <bstavroulakis@gmail.com>
2018-02-09 00:21:09 -0800

    add newsletter save

--- handlers/newsletters/generateNewsletter.js
+++ handlers/newsletters/generateNewsletter.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const fs = require('fs-extra');
 const sanitize = require('mongo-sanitize');
+const GitHub = require('github-api');
 
 const calendar = require('../../helpers/calendar.helper');
 const config = require('../../config');
@@ -8,6 +9,7 @@ const config = require('../../config');
 const Newsletter = require('../../src/components/newsletter');
 
 const generateNewsletter = function(req, callback) {
+  const saveNewsletter = sanitize(req.body.save);
 	const links = sanitize(req.body.links);
 	const today = new Date(Date.now());
 	const week = process.env.week || calendar.getWeek(today);
@@ -17,7 +19,37 @@ const generateNewsletter = function(req, callback) {
 
 	const newsletterHtml = newsletter.generate();
 
-	callback.onSuccess(newsletterHtml);
+	if (!saveNewsletter) {
+		callback.onSuccess(newsletterHtml);
+
+		return;
+	}
+	
+	var gh = new GitHub({
+		token: process.env.GITHUB_TOKEN
+ });
+
+ const repo = gh.getRepo("dotnetweekly", "dnw-newsletter");
+ repo.createBranch("master", `newsletter-${year}-${week}`).then(() => {
+	repo.writeFile(
+		`newsletter-${year}-${week}`,
+		`public/issues/${year}/${week}/index.html`, 
+		newsletterHtml,
+		`Newsletter - Week: ${week} Year: ${year}`,
+		{ encode: true }
+	 ).then(response => {
+		repo.createPullRequest({
+			title: `Newsletter - Week: ${week} Year: ${year}`,
+			body: `Newsletter - Week: ${week} Year: ${year}`,
+			head: `newsletter-${year}-${week}`,
+			base: "master"
+		})
+		.then()
+	 })
+	 .catch(error => {
+		 console.log(error);
+	 })
+ })
 };
 
 module.exports = generateNewsletter;
