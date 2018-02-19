d13a8389216d9862bda4c7015974918129324019
bstavroulakis <bstavroulakis@gmail.com>
2018-02-18 21:10:38 -0800

    updated build process

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,8 +1,8 @@
 
-ls
-cd ../../
-ls
-cp -R "repository/*" "wwwroot/"
-cd "wwwroot"
-ls
-npm install --only=production
+eval 'ls'
+eval 'cd ../../'
+eval 'ls'
+eval 'cp -R "repository/*" "wwwroot/"'
+eval 'cd "wwwroot"'
+eval 'ls'
+eval 'npm install --only=production'
